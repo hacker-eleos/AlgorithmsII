a14f9e4f7e94c99166d1fe2f5bc82462a3a4e04a
hacker-eleos <vikrambhatt1405@gmail.com>
2021-04-18 21:08:29 +0530

    working version 2

--- seam/DirectedEdge.java
+++ seam/DirectedEdge.java
@@ -4,9 +4,25 @@
  *  Description:
  **************************************************************************** */
 
-package PACKAGE_NAME;
 public class DirectedEdge {
-    public static void main(String[] args) {
-        
+    private final int v;
+    private final int w;
+
+    public DirectedEdge(int v, int w) {
+        this.v = v;
+        this.w = w;
+
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public String toString() {
+        return String.format("%d->%d ", v, w);
     }
 }
